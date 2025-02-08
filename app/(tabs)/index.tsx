7ea9dd04e5bb4c864ca28301d0db8a39d363caad
Siddharth Doshi <sidoshi510@gmail.com>
2025-02-08 20:26:01 +0100

    first screen

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,21 @@
-import { Link } from "expo-router";
 import { StyleSheet, Text, View } from "react-native";
+
+import ImageViewer from "@/components/ImageViewer";
+import Button from "@/components/Button";
+
+const PlaceHolderImage = require("@/assets/images/background-image.png");
 
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Edit to edit this screen.</Text>
+      <View style={styles.imageContainer}>
+        <ImageViewer imgSource={PlaceHolderImage} />
+      </View>
 
-      <Link href="/about" style={styles.button}>
-        Go to About screen
-      </Link>
+      <View style={styles.footerContainer}>
+        <Button theme="primary" label="Choose a photo" />
+        <Button label="Use this photo" />
+      </View>
     </View>
   );
 }
@@ -16,17 +23,17 @@ export default function Index() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
     alignItems: "center",
     backgroundColor: "#25292e",
   },
-  text: {
-    color: "#fff",
+
+  imageContainer: {
+    flex: 1,
+    paddingTop: 28,
   },
 
-  button: {
-    fontSize: 20,
-    textDecorationLine: "underline",
-    color: "#fff",
+  footerContainer: {
+    flex: 0.5,
+    alignItems: "center",
   },
 });
