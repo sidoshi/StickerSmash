cd8d98aafe74ebcb898fa3ec6562d8b8ef07f000
Siddharth Doshi <sidoshi510@gmail.com>
2025-02-08 20:00:09 +0100

    basic tabs

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,13 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Stack>
+      <Stack.Screen
+        name="(tabs)"
+        options={{ title: "App", headerShown: false }}
+      />
+      <Stack.Screen name="+not-found" />
+    </Stack>
+  );
 }
